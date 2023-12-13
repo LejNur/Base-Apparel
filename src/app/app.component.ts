2ded808879cf7a36945223cf43deffee7c528ecb
Lejla <leejlaa.73@gmail.com>
2023-12-13 12:11:28 +0100

    final v

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'front-end-mentor';
+
+ 
+
+    onSubmit(form: NgForm) {
+      console.log(form.value) 
+    }
+
+
+  
 }
